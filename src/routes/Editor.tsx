96d17612eee0f908e74b85271b8762e165332c03
Jeff <gibgambo@gmail.com>
2026-08-13 02:07:54 +0200

    TailwindCSS installed, Editor TSX angepasst

--- src/routes/Editor.tsx
+++ src/routes/Editor.tsx
@@ -1,3 +1,7 @@
 export function Editor() {
-  return <h1>Editor</h1>;
+  return (
+    <div className="min-h-screen bg-onilo-background flex items-center justify-center">
+      <h1 className="text-onilo-primary text-3xl font-bold">Editor</h1>
+    </div>
+  );
 }
