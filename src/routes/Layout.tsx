e6535993503d8eedaab76db3b2c99022bb6b6e8f
Jeff <gibgambo@gmail.com>
2026-08-15 00:12:13 +0200

    CodeReviewTicket12: Umbennung CSS Klasse, Coding Standard Umbennung

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,18 +1,18 @@
 import { NavLink, Outlet } from "react-router";
 
-const navLinkClassName = ({ isActive }: { isActive: boolean }) =>
+const getNavLinkClassName = ({ isActive }: { isActive: boolean }) =>
   `px-3 py-2 text-lg font-semibold ${
-    isActive ? "text-white border-b-2 border-onilo-accent" : "text-white hover:text-onilo-background-dark"
+    isActive ? "text-white border-b-2 border-onilo-accent" : "text-white hover:text-onilo-nav-hover"
   }`;
 
 export function Layout() {
   return (
     <>
       <header className="bg-onilo-primary flex gap-4 px-6 py-4 justify-center">
-        <NavLink to="/editor" className={navLinkClassName}>
+        <NavLink to="/editor" className={getNavLinkClassName}>
           Editor
         </NavLink>
-        <NavLink to="/recordings" className={navLinkClassName}>
+        <NavLink to="/recordings" className={getNavLinkClassName}>
           Recordings-Dashboard
         </NavLink>
       </header>
